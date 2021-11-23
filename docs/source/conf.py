83c870c89ea530abd12eca9d3900fd2892e78ceb
Yury Lysogorskiy <yura.lysogorskii@gmail.com>
2021-11-23 13:36:33 +0100

    update theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_material'
+html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
     "sidebar_hide_name": True,
